1ea6eaa3e57a89825e82fd291e625540f8c7d6c5
likun@taibrain.com <12345678>
2019-12-30 18:22:35 +0800

    commit lk

--- dubbo-service/dubbo-service-consumer/src/main/java/com/dubbo/example/consumer/action/DemoAction.java
+++ dubbo-service/dubbo-service-consumer/src/main/java/com/dubbo/example/consumer/action/DemoAction.java
@@ -1,12 +1,14 @@
 package com.dubbo.example.consumer.action;
 
 import com.dubbo.example.DemoService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class DemoAction {
 
     @Reference(version = "1.0.0")
@@ -15,6 +17,7 @@ public class DemoAction {
     @GetMapping("getName")
     public String getName() {
         String li = demoService.sayHello("li");
+        log.info("-------:{}", li);
         return li;
     }
 }
